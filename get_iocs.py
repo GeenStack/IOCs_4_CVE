b6c91e3d2f68cc5abb5a22e7e08ae3f2578d8b5f
Geenstack <hayleytops@gmail.com>
2022-03-28 22:41:18 +0300

    Костыль с репортом и статистикой по добытым IOCs

--- get_iocs.py
+++ get_iocs.py
@@ -10,6 +10,7 @@ def get_iocs(cve_id):
     data = r.json()
     indicators = []
     indicators_id = []
+    indicators_types = {}
     pulses = data["pulse_info"]["pulses"]
     if pulses:
         for pulse in pulses:
@@ -20,6 +21,7 @@ def get_iocs(cve_id):
                 if pulse_indicators:
                     for indicator in pulse_indicators:
                         if indicator["type"] != "CVE":
+
                             if not check_ioc_in_db(cve_id, indicator["id"]):
                                 query = "INSERT INTO iocs_for_cve VALUES (NULL, '{}', '{}', '{}', '{}', '{}')"
 
@@ -30,12 +32,27 @@ def get_iocs(cve_id):
                                                    "type":indicator["type"],
                                                    "content":indicator["content"]})
                                 indicators_id.append(indicator["id"])
+
+                                if indicator["type"] not in indicators_types:
+                                    indicators_types.update({"{}".format(indicator["type"]):1})
+                                else:
+                                    indicators_types[indicator["type"]] += 1
                         else:
                             pass
             except Exception as e:
                 print(query)
                 continue
 
-        return {"indicators":indicators, "id_list":indicators_id}
+        #Доработать отчет.
+        #Создавать файл репорта в JSON, который потом и будет отправляться
+        #И что делать, если отчет пустой - не возвращать репорт
+        #
+        #
+        #
+        report = "Найдены новые IOCs, связанные с {}\n".format(cve_id)
+        for indicators_type in indicators_types:
+            report += "{} - {}\n".format(indicators_type, str(indicators_types[indicators_type]))
+        print(report)
+        return {"indicators":indicators, "id_list":indicators_id, "types_count":indicators_types}
     else:
             return False
